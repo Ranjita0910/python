eb4b4f3d8a0e1c72305f072a2ec2fd72587070a7
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-02 23:06:18 +0530

    Add files via upload

---
+++ conditional_ statement.py.py
@@ -0,0 +1,54 @@
+###conditional statements###
+## simple example1
+
+a=int(input('even number:'))
+if a%2==0:
+        print(a,'even number')
+      
+else:
+        print(a,'odd number')
+
+##simple example 2
+number1=10
+number2=20
+
+if number1>number2:
+        print('yes')
+
+elif number1==number2:
+        print('nothing')
+
+else:
+        print('no')
+
+## example 3
+x=int(input('x:'))
+y=int(input('y:'))
+
+if x>y:
+        print('x is greater than y')
+
+elif x==y:
+        print('x is equal to y')
+
+else:
+        print('x is less than y')
+
+
+###example 4
+print('rain fall check')
+rain=int(input('rain fall:'))
+if rain>150:
+        print('red alert')
+
+elif rain==100:
+        print('orange alert')
+
+else:
+        print('yellow alert')
+
+    
+        
+
+
+        
