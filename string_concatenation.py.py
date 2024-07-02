ecd4360bde620b738300e5601b57b5d5bd2694ec
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-02 22:50:14 +0530

    Add files via upload

---
+++ string_concatenation.py.py
@@ -0,0 +1,26 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ## string concatenation (+)
+>>> ## using a plus symbol we combined many sting satement
+>>> a='ranju'
+>>> b='good'
+>>> c='girl'
+>>> print(a+b+c)
+ranjugoodgirl
+>>> ##we use space between world
+>>> print(a+' '+b+' '+c)
+ranju good girl
+>>> 
+>>> ##example 2
+>>> a='good morning everyone'
+>>> b='i am presenting python'
+>>> print(a+b)
+good morning everyonei am presenting python
+>>> 
+>>> #example 3
+>>> a='i am from haveri'
+>>> b=a
+>>> b=a
+>>> b='my name ranjita'
+>>> print(a+b)
+i am from haverimy name ranjita
