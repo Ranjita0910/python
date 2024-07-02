76fc4b6232545de4234e2bf4b3e8d6edb1b61cac
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-02 22:55:05 +0530

    Add files via upload

---
+++ list library1.py
@@ -0,0 +1,50 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ###list ####
+... list=[1,2,3,4,5,6,7,8,9]
+... print(list)
+... [1, 2, 3, 4, 5, 6, 7, 8, 9]
+... 
+... type(list)
+... <class 'list'>
+... 
+... ##indexing of list#
+... #indexing
+... list=[1,2,3,4,5,6,7,8,9]
+... print(list[2])
+... 3
+... 
+... print(list[0])
+... 1
+... 
+... print(list[1])
+... 2
+... 
+... print(list[2])
+... 3
+... 
+... print(list[4])
+... 5
+... 
+... 
+... ##reverse indexing#
+... list=[1,2,3,4,5,6,7,8,9]
+... print(list[-1])
+... 9
+... 
+... print(list[-2])
+... 8
+... 
+... print(list[-3])
+... 7
+... 
+... print(list[-4])
+... 6
+
+
+##lenth of list##
+list=[1,2,3,4,5,6,7,8,9]
+print(len(list))
+9
+
+SyntaxError: multiple statements found while compiling a single statement
