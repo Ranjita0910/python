971d8de09b997dc4dc45c1b6580fc230711b081d
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-02 22:52:50 +0530

    Add files via upload

---
+++ string _slicing.py.py
@@ -0,0 +1,24 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ###slicing string
+>>> a="ranjita"
+>>> print(a[0:2])
+ra
+>>> #it will consider (x-1)
+>>> print(a[0:4])
+ranj
+>>> 
+>>> ##it has inbuilt or default ranging from 1st to last
+>>> a='haveri'
+>>> print(a[0:])
+haveri
+>>> #last point take default
+>>> 
+>>> print(a[:5])
+haver
+>>> #1st one is also inbuilt
+>>> 
+>>> #1st and last also take inbuilt
+>>> 
+>>> print(a[:])
+haveri
