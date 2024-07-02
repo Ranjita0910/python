bfda569e70bceb8912c0f708bb05416fbf0efdaf
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-02 22:36:32 +0530

    Add files via upload

---
+++ variable_text.py.py
@@ -0,0 +1,38 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> variable test
+SyntaxError: invalid syntax
+>>> a=34
+>>> print(a)
+34
+>>> b=56
+>>> print(b)
+56
+>>> c="ranjita"
+>>> print(c)
+ranjita
+>>> d="hai good evening"
+>>> print(d)
+hai good evening
+>>> message="okay"
+>>> print(message)
+okay
+>>> B =6
+>>> print(B)
+6
+>>> A-B=45
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> a-=45
+>>> print(a-)
+SyntaxError: invalid syntax
+>>> _a=34
+>>> print(_a)
+34
+>>> _v_f
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    _v_f
+NameError: name '_v_f' is not defined
+>>> _a_b=78
+>>> print(_a_b)
+78
