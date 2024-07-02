d88a8eace863187ebd145281856a59666490e3b6
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-02 23:13:49 +0530

    Add files via upload

---
+++ LOOPING.py
@@ -0,0 +1,14 @@
+a=0
+while a<=4:
+        print('B', end=' ')
+        a+=1
+        b=0
+        while b<=3:
+            print('T', end=' ')
+            b+=1
+            print()
+
+            
+                
+    
+    
