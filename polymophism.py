4cbb124798dd2b0b55400ef7c15428b210b3f806
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-07 14:44:05 +0530

    Add files via upload

---
+++ polymophism.py
@@ -0,0 +1,34 @@
+#polymophism
+# class logic:
+#     def math(self,*args):
+#         if len(args)==1:
+#             return args[0]**2
+#         elif len(args)==2:
+#             return args[0]+args[1]
+#         elif len(args)==3:
+#             return args[0]+args[1]+args[2]
+#         else:
+#             return "invalid"
+# calc=logic()
+# print(calc.math(3))
+# print(calc.math(3,5))
+# print(calc.math(2,3,4))
+# print(calc.math(2,3,4,5))
+
+# class and object example
+# class cricket():
+#     def match(self,score) :
+#         if score==150:
+#             return "win"
+#         elif score==100:
+#             return "lost"
+#         else:
+#             return "come out"
+# outcome=cricket()
+# print(outcome.match(150))
+# print(outcome.match(100))
+# print(outcome.match(40))
+
+# magic method find 
+# int()=ctr
+
