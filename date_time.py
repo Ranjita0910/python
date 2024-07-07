9d8c76ed4587ffa1eb1a323d81c6b99f5d5c65cc
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-07 14:38:38 +0530

    Add files via upload

---
+++ date_time.py
@@ -0,0 +1,12 @@
+# import datetime
+# #current date and time
+# now=datetime.datetime.now()
+# print("curent date and time:",now)
+
+# #current date
+# today=datetime.date.today()
+# print("current time:",today)
+
+# #current time
+# currenttime =datetime.datetime.now().time()
+# print("current time:",currenttime)
