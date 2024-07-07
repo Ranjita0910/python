44d32652bd590406dab5e3fc14de3ca04ed85ea0
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-07 14:40:48 +0530

    Add files via upload

---
+++ file_handling (2).py
@@ -0,0 +1,36 @@
+# file=open("file_handling.py")
+# counter=file.read()
+# print(counter)
+
+#write funtion in file_handling
+# file=open("file_handling.py","w")
+# file.write("meet me")
+
+# appen the new data
+# file=open("file_handling.py","a")
+# file.write("\n okay sure ")
+
+
+# for read and write"x"
+# file=open("file_handling.py","x")
+# file.write("new line using x")
+
+# a   -  append
+# r   -  read
+# w   -  write
+# x   - read and write
+# b   - binary 
+
+#close file
+# file.close():to save ram we use close for every fuction here
+
+#to create new folder
+# file=open("myfolder.txt","x")
+# remove
+# import os
+# os.remove("folder name")
+# #to direct open folder
+# import os
+# os.mkdir("folder name")
+# remove
+# os.rmdir("folder name")
