0d0e10c51789defe03a695ee305c854352d4bf4f
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-07 14:36:06 +0530

    Add files via upload

---
+++ Classes_and_object.py
@@ -0,0 +1,38 @@
+# class computer:
+#     def config(self):
+#         print('cpu:i5 & RAM:8GB')
+# c1=computer()
+# c2=computer()
+# print(type(c1))
+# print(type(c2))
+# print(id(c1))
+# print(id(c2))
+
+# class frd:
+#     a1="ravi"
+#     b1="raju"
+#     def best(self):
+#         print("my best frd",self.a1)
+#         print("my best frd",self.b1)
+# close= frd()
+# print(close.a1)
+# close.best()
+
+#__init__
+# class computer:
+#     def__init__(self,model,cpu):
+#         self.model=Model
+#         self.cpu= cpu
+# c1=computer(2021,"i3")
+# c2=computer(2021,"i3")
+# if c1==c2:
+#     print('same')
+# else:
+#     print("different")
+# if c1.model==c2.model:
+#     print("same")
+# else:
+#     print("different")
+
+
+
